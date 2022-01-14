964f5707d99ac27e9acf2ef3908d8f0c0ebdfd2b
Gal Zur <galzur01@gmail.com>
2022-01-14 14:24:08 +0200

    Added generic Queue

---
+++ src/Queue.java
@@ -0,0 +1,28 @@
+public class Queue<T> {
+    private LinkedList<T> first=null;
+    private LinkedList<T> last=null;
+    public void insert(T x){
+        LinkedList<T> temp = new LinkedList<>(null, null, x);
+        if(first==null)
+            first=temp;
+        else {
+            temp.setPrev(last);
+            last.setNext(temp);
+        }
+        last = temp;
+    }
+    public T remove(){
+        if(first == null)
+            return null;
+        T x = first.getValue();
+        first = first.getNext();
+        if(first==null)
+            last=null;
+        return x;
+    }
+    public T head(){
+        if(first==null)
+            return null;
+        return first.getValue();
+    }
+}
